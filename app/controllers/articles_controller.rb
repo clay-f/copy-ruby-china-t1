34438dc8e301507197e336776d8bb63c32f881f0
lyf-t <liyongfeit@gmail.com>
2016-11-27 21:34:00 +0800

    article ARUD is completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,53 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @article = Article.all.order("created_at DESC")
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params[:article].permit(:title, :text)
+    end
+
 end
