471bd95fc369117e71df454b3d4d18544594e310
lyf-t <liyongfeit@gmail.com>
2016-12-01 14:00:16 +0800

    need add image options on article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'jobs/index'
+
   devise_for :users
   resources :articles do
     resources :comments, except: [:index]
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :topics, only: [:show, :index]
   resources :sites, only: [:index]
+  resources :jobs, only: [:index]
 
   root 'articles#index'
 
