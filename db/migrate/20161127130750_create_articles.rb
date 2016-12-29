cb5e8a9e4311ba3d22e7b8b3335cb0f92811d6c4
F <liyongfeit@gmail.com>
2016-12-29 20:29:40 +0800

    add comments

--- db/migrate/20161127130750_create_articles.rb
+++ db/migrate/20161127130750_create_articles.rb
@@ -5,6 +5,11 @@ class CreateArticles < ActiveRecord::Migration[5.0]
       t.text :text
 
       t.timestamps
+
+      def change
+        create_table :comments do |t|
+        end
+      end
     end
   end
 end
