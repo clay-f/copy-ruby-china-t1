4f0ae7db03eee4b4bff68fc250e0f32c23da4f78
F <liyongfeit@gmail.com>
2016-12-29 21:56:54 +0800

    remove comments

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 20161229134605) do
     t.integer  "article_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.integer  "raty"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "create_products", force: :cascade do |t|
     t.string   "name"
     t.text     "text"
