a6c0081243760b32639ea3e8b2b50532947fc7b4
F <liyongfeit@gmail.com>
2016-12-29 20:45:48 +0800

    config user_id article_id to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229123300) do
+ActiveRecord::Schema.define(version: 20161229124457) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20161229123300) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "article_id"
+    t.integer  "user_id"
   end
 
   create_table "create_products", force: :cascade do |t|
