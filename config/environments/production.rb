a8c9cc7a6316f453ff9fc667a64476341dea1956
F <liyongfeit@gmail.com>
2017-01-14 17:49:32 +0800

    add assets compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  #
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -83,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.compile = true
+  config.assets.digest = true
 end
