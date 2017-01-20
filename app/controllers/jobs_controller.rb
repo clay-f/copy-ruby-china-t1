5a5d61bad0b70c11bdac49bd2acad536f13599db
F <liyongfeit@gmail.com>
2017-01-20 15:36:33 +0800

    repair 'new topic' name to 'new article'

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,11 @@
 class JobsController < ApplicationController
+  before_action :random_suggestions_text
+
   def index
     @article = Article.all.order("created_at DESC").paginate(page: params[:page], :per_page => 7)
+  end
+
+  def random_suggestions_text
+    @random_text = SuggestionsText.all.shuffle[1].text
   end
 end
